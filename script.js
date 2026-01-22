9ac917633cad1eeb37b7267855963b937f500066
Andy Agyei-Dwumah <andydwumah@gmail.com>
2026-01-22 15:07:50 +0000

    update game logic
    - Implement number of lives remaining logic
    - Add 10 second delay before restarting a game
    - Keep @type for only first element in script.js. Remove from the rest
    -

--- script.js
+++ script.js
@@ -9,46 +9,47 @@ const myObj = {
   green: 0,
 };
 
-const boardSize = 4;
 let levelVal = 1; // The level the user is currently on
 let numOfSquares = 3; // The default number of squares to generate for level 1
+let numOfLivesRemaining = 3;
+const boardSize = 4;
 const endGameText = "End Game";
 const nextLevelText = "Next";
+const numOfLivesRemainingText = "Number of lives remaining:";
+const delayBeforeRestartingALevel = 10;
+/* 
+  How long it takes for a random coloured square to appear on the board
+  How long it takes before a random coloured square to disappears of the board
+*/
+const delayTime = 1000;
 
 // ===================================================
+// Can add `@type` to all elements
 /** @type {HTMLSpanElement} */
 const year = document.querySelector(".year");
 year.textContent = new Date().getFullYear();
-/** @type {HTMLDivElement} */
 const homePageWrapper = document.querySelector(".js-home-page-wrapper");
-/** @type {HTMLButtonElement} */
 const startGameBtn = document.querySelector(".js-button--play-game");
-/** @type {HTMLDivElement} */
 const gameWrapper = document.querySelector(".js-game-wrapper");
-/** @type {HTMLHeadingElement} */
 const levelText = document.querySelector(".js-game-wrapper__heading");
-/** @type {HTMLInputElement} */
 const redInput = document.querySelector("#red-squares");
-/** @type {HTMLInputElement} */
 const blueInput = document.querySelector("#blue-squares");
-/** @type {HTMLInputElement} */
 const greenInput = document.querySelector("#green-squares");
-/** @type {HTMLButtonElement} */
 const submitBtn = document.querySelector(".js-button--submit");
-/** @type {HTMLFormElement} */
 const answerForm = document.querySelector(".js-answer-form");
-/** @type {HTMLParagraphElement} */
 const correctIncorrectText = document.querySelector(
   ".js-correct-incorrect-text",
 );
-/** @type {HTMLDivElement} */
 const correctIncorrectTextWrapper = document.querySelector(
   ".js-correct-incorrect-text-wrapper",
 );
-/** @type {HTMLButtonElement} */
-const nextLevelBtn = document.querySelector(".js-button--next-level");
-/** @type {HTMLTableElement} */
+const nextLvlOrEndGameBtn = document.querySelector(
+  ".js-button--next-lvl-or-end-game",
+);
 const table = document.querySelector(".js-table");
+const numOfLivesHeading = document.querySelector(
+  ".js-game-wrapper__num-of-lives-heading",
+);
 // ===================================================
 const enableInputFields = () => {
   // Enable input fields and button
@@ -56,8 +57,8 @@ const enableInputFields = () => {
   blueInput.removeAttribute("disabled");
   greenInput.removeAttribute("disabled");
   submitBtn.removeAttribute("disabled");
-  // * DISPLAY OBJECT IN CONSOLE
-  // console.log("My object", myObj);
+  // ! UNCOMMENT TO DISPLAY OBJECT IN CONSOLE
+  console.log("My object", myObj);
 };
 
 const generateSquares = () => {
@@ -65,7 +66,7 @@ const generateSquares = () => {
     setTimeout(
       () => {
         if (i === numOfSquares - 1) {
-          setTimeout(enableInputFields, (i + 1) * 1000);
+          setTimeout(enableInputFields, (i + 1) * delayTime);
         }
         const randomSquare =
           table.getElementsByTagName("td")[getRandomTableCellIndex(boardSize)];
@@ -89,9 +90,9 @@ const generateSquares = () => {
           } else {
             randomSquare.classList.remove("green");
           }
-        }, 1000);
+        }, delayTime);
       },
-      (i + 1) * 1000,
+      (i + 1) * delayTime,
     );
   }
 };
@@ -101,26 +102,30 @@ const startGame = () => {
   homePageWrapper.style.display = "none";
   // Display game page
   gameWrapper.style.display = "flex";
-  // Update 'LEVEL' text
+  // Update level text
   levelText.innerText = `Level ${levelVal}`;
-  // Start generating randoms squares
+  // Update 'number of lives remaining text'
+  numOfLivesHeading.textContent = `${numOfLivesRemainingText} ${numOfLivesRemaining}`;
+  // Start generating random squares
   generateSquares();
 };
 
-const resetStyles = (levelTextTextContent = "", answerWasCorrect = true) => {
+const resetStyles = (levelTextTextContent = "", endOfGame = false) => {
   // Reset Object
   myObj.red = 0;
   myObj.green = 0;
   myObj.blue = 0;
-  if (!answerWasCorrect) {
+  if (!endOfGame) {
     // Hide Game page
     gameWrapper.style.display = "none";
     // Display home page
     homePageWrapper.style.display = "block";
     // Reset level
     levelVal = 1;
+    // Reset number of lives remaining
+    numOfLivesRemaining = 3;
   }
-  // Update Level text
+  // Update level text
   levelText.textContent = levelTextTextContent;
   // Reset input fields and set to disabled
   redInput.value = "0";
@@ -132,21 +137,21 @@ const resetStyles = (levelTextTextContent = "", answerWasCorrect = true) => {
   // Hide 'correct incorrect text' and next level button
   correctIncorrectTextWrapper.classList.add("hidden");
   correctIncorrectText.textContent = "";
-  nextLevelBtn.classList.add("hidden");
+  nextLvlOrEndGameBtn.classList.add("hidden");
 };
 
 // ===================================================
 startGameBtn.addEventListener("click", startGame);
-nextLevelBtn.addEventListener("click", () => {
-  if (nextLevelBtn.textContent === endGameText) {
-    resetStyles("", false);
+nextLvlOrEndGameBtn.addEventListener("click", () => {
+  if (nextLvlOrEndGameBtn.textContent === endGameText) {
+    resetStyles("");
     // Reset number of squares
     numOfSquares = 3;
-  } else if (nextLevelBtn.textContent === nextLevelText) {
-    resetStyles(`Level ${++levelVal}`);
-    // Generate next game squares
+  } else if (nextLvlOrEndGameBtn.textContent === nextLevelText) {
+    resetStyles(`Level ${++levelVal}`, true);
     // Increase number of squares by 1
     ++numOfSquares;
+    // Generate next game squares
     generateSquares();
   } else {
     return;
@@ -154,6 +159,7 @@ nextLevelBtn.addEventListener("click", () => {
 });
 answerForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  // Correct answer
   if (
     +redInput.value === myObj.red &&
     +blueInput.value === myObj.blue &&
@@ -161,13 +167,53 @@ answerForm.addEventListener("submit", (e) => {
   ) {
     correctIncorrectTextWrapper.classList.remove("hidden");
     correctIncorrectText.textContent = "Congratulations!";
-    nextLevelBtn.classList.remove("hidden");
-    nextLevelBtn.textContent = nextLevelText;
+    nextLvlOrEndGameBtn.classList.remove("hidden");
+    nextLvlOrEndGameBtn.textContent = nextLevelText;
+    // Wrong answer
   } else {
-    correctIncorrectTextWrapper.classList.remove("hidden");
-    correctIncorrectText.textContent = "Incorrect!";
-    nextLevelBtn.classList.remove("hidden");
-    nextLevelBtn.textContent = endGameText;
+    --numOfLivesRemaining;
+    if (numOfLivesRemaining > 0) {
+      // Reset input fields and set to disabled
+      redInput.setAttribute("disabled", true);
+      blueInput.setAttribute("disabled", true);
+      greenInput.setAttribute("disabled", true);
+      redInput.value = "0";
+      blueInput.value = "0";
+      greenInput.value = "0";
+      // Reset myObj to 0
+      myObj.red = 0;
+      myObj.blue = 0;
+      myObj.green = 0;
+      // Show incorrect text at the bottom
+      correctIncorrectTextWrapper.classList.remove("hidden");
+      correctIncorrectText.textContent = "Incorrect!";
+      // Update number of lives remaining text
+      numOfLivesHeading.textContent = `${numOfLivesRemainingText} ${numOfLivesRemaining}`;
+      // Dynamically show when the level will be restarted
+      for (let i = delayBeforeRestartingALevel; i >= 0; i--) {
+        setTimeout(
+          () => {
+            if (i === 0) {
+              // On last iteration generate squares and hide text content
+              correctIncorrectTextWrapper.classList.add("hidden");
+              correctIncorrectText.textContent = "";
+              generateSquares();
+            } else {
+              // Dynamically show text at bottom of game board
+              correctIncorrectText.textContent = `Incorrect! Restarting level in ${i} seconds...`;
+            }
+          },
+          (delayBeforeRestartingALevel + 1 - i) * 1000, // Count up from 0 onwards
+        );
+      }
+    } else {
+      // No lives remaining
+      numOfLivesHeading.textContent = `${numOfLivesRemainingText} ${numOfLivesRemaining}`;
+      correctIncorrectTextWrapper.classList.remove("hidden");
+      correctIncorrectText.textContent = "Game over!";
+      nextLvlOrEndGameBtn.classList.remove("hidden");
+      nextLvlOrEndGameBtn.textContent = endGameText;
+    }
   }
   // Disable submit button to prevent another form submission
   submitBtn.setAttribute("disabled", true);
